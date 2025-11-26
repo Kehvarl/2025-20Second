3362b754abeec3b82026346e4c924e35ebf26e02
Kehvarl <Kehvarl@Kehvarl.com>
2025-11-25 19:49:13 -0700

    game over screen

--- app/main.rb
+++ app/main.rb
@@ -30,6 +30,7 @@ end
 
 
 def init args
+  args.state.game_over = false
   args.state.switches = []
   (0...5).each do |x|
     args.state.switches << Toggle_Switch.new({x:x*50 + 220,y:640})
@@ -61,13 +62,32 @@ def calculate args
 end
 
 def get_timer args
-  "%.2f" % (args.state.count_to - Time.now)
+  timer = args.state.count_to - Time.now
+  if timer <= 0.00
+    timer = 0.00
+    args.state.game_over = true
+  end
+  "%.2f" % timer
 end
+
+def game_over_tick args
+    args.outputs.primitives << {x:0, y:0, w:720, h:1280, r:0, g:0, b:0}.solid!
+    args.outputs.primitives << {x:280, y:800, w:50, h:50, r:0, g:196, b:0, size_enum: 30, text:"GAME"}.label!
+    args.outputs.primitives << {x:280, y:700, w:50, h:50, r:0, g:196, b:0, size_enum: 30, text:"OVER"}.label!
+    args.outputs.primitives << {x:255, y:600, w:50, h:50, r:255, g:196, b:0, size_enum: 16, text:"You Lose!"}.label!
+
+end
+
 
 def tick args
   if Kernel.tick_count <= 0
       init args
   end
+  if args.state.game_over
+    game_over_tick(args)
+    return
+  end
+
   args.state.switches.each {|s| s.tick(args)}
   args.state.button.tick(args)
 
